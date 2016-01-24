3618468daabe6a0b8e9841ce8754b474f0b20b13
accraze <accraze@gmail.com>
2016-01-24 08:55:00 -0800

    feat: audit package for vulnerable deps

--- src/index.js
+++ src/index.js
@@ -1,5 +1,16 @@
 const SRError = require('@semantic-release/error')
+var auditPackage = require('nsp/lib/auditPackage.js')
 
-module.exports = function (pluginConfig, config, cb) {
-  cb(null)
+module.exports = function (pluginConfig, packagePath, cb) {
+  if (!packagePath) {
+    packagePath = process.cwd() + '/package.json'
+  }
+
+  auditPackage(packagePath, (err, results) => {
+    if (err) return cb(new SRError('nsp returned unexpected error code', 'ENSPFAIL'))
+
+    if (results.length > 0) return cb(new SRError('Vulnerable Dependencies', 'EVULNERABLEDEPS'))
+
+    return cb(null)
+  })
 }
--- test/specs/index.js
+++ test/specs/index.js
@@ -3,10 +3,40 @@ const { test } = require('tap')
 const SRError = require('@semantic-release/error')
 
 const condition = proxyquire('../../', {
-  // ...
+  'auditPackage': (cb) => cb(null)
 })
 
-test('run-script', (t) => {
-  t.ok(condition)
-  t.end()
+test('find vulnerable packages', (tt) => {
+  tt.plan(2)
+
+  condition({}, 'test/data/vulnerable-package.json', (err, results) => {
+    tt.ok(err instanceof SRError)
+    tt.is(err.code, 'EVULNERABLEDEPS')
+  })
 })
+
+test('does not raise error on safe packages', (tt) => {
+  tt.plan(1)
+
+  condition({}, 'test/data/dep-package.json', (err) => {
+    tt.is(err, null)
+  })
+})
+
+test('requires proper path to package', (tt) => {
+  tt.plan(2)
+
+  condition({}, 'weird', (err) => {
+    tt.ok(err instanceof SRError)
+    tt.is(err.code, 'ENSPFAIL')
+  })
+})
+
+test('if no path given, path defaults to cwd + package.json', (tt) => {
+  tt.plan(1)
+
+  condition({}, '', (err) => {
+    tt.is(err, null)
+  })
+})
+
